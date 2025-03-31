da2327ae6090db5b3b2edb1c8933b74bf4e23aa9
aleena-joseph-86 <jaleena727@gmail.com>
2025-03-31 16:33:42 +0530

    Replaced postgreSQL with mySQL

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,35 @@
-import { Pool } from "pg";
+import mysql from "mysql2/promise";
 
-const pool = new Pool({
-  user: process.env.POSTGRESQL_ADDON_USER,
-  host: process.env.POSTGRESQL_ADDON_HOST,
-  database: process.env.POSTGRESQL_ADDON_DB,
-  password: process.env.POSTGRESQL_ADDON_PASSWORD,
-  port: Number(process.env.POSTGRESQL_ADDON_PORT),
-  ssl: { rejectUnauthorized: false },
-});
+let pool: mysql.Pool;
 
-export const query = (text: string, params?: any[]) => pool.query(text, params);
+export default async function getPool(): Promise<mysql.Pool> {
+  if (pool) {
+    return pool;
+  }
+
+  try {
+    pool = mysql.createPool({
+      host: process.env.MYSQL_HOST,
+      port: process.env.MYSQL_PORT
+        ? parseInt(process.env.MYSQL_PORT)
+        : undefined,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      waitForConnections: true,
+      connectionLimit: 10,
+    });
+    return pool;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function executeQuery(query: string, params: any[]): Promise<any> {
+  const pool = await getPool();
+  const connection = await pool.getConnection();
+  const results = await connection.execute(query, params);
+  connection.release();
+  return results;
+}
