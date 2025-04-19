3e13cc85dd9a2ede59a44dc1ee387357f66977ca
visalinikamaraj <vishalinikamaraj286@gmail.com>
2025-04-19 20:49:35 +0530

    update UI styling for Profile page

--- src/app/components/sideBar/sideBar.tsx
+++ src/app/components/sideBar/sideBar.tsx
@@ -2,6 +2,7 @@
 
 import styles from "./sideBar.module.scss";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { AiFillHome } from "react-icons/ai";
 import { FaRobot } from "react-icons/fa";
 import { FaUserCircle } from "react-icons/fa";
@@ -9,14 +10,50 @@ import { MdCalendarToday } from "react-icons/md";
 import { FaHandshake } from "react-icons/fa";
 
 export default function Sidebar() {
+  const pathname = usePathname();
   return (
     <div className={styles.sidebar}>
       <ul>
-        <Link href="/home"><AiFillHome />Home</Link>
-        <Link href="/bot"><FaRobot />Bot</Link>
-        <Link href="/profile"><FaUserCircle />Profile</Link>
-        <Link href="/calendar"> <MdCalendarToday />My Calendar</Link>
-        <Link href="/meetings"><FaHandshake />Meetings</Link>
+        <li>
+          <Link
+            href="/home"
+            className={pathname === "/home" ? styles.activeLink : ""}
+          >
+            <AiFillHome /> Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/bot"
+            className={pathname === "/bot" ? styles.activeLink : ""}
+          >
+            <FaRobot /> Bot
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/profile"
+            className={pathname === "/profile" ? styles.activeLink : ""}
+          >
+            <FaUserCircle /> Profile
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/calendar"
+            className={pathname === "/calendar" ? styles.activeLink : ""}
+          >
+            <MdCalendarToday /> My Calendar
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/meetings"
+            className={pathname === "/meetings" ? styles.activeLink : ""}
+          >
+            <FaHandshake /> Meetings
+          </Link>
+        </li>
       </ul>
     </div>
   );
